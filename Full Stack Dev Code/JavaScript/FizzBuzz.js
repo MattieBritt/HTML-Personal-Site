e20fe0410b478f615f37887c0904b972da46de42
Mattie Britt <MattieBritt@users.noreply.github.com>
2023-02-28 23:04:10 -0700

    02/28/2023 - Updated Files, Arrays
    I'm currently developing my understanding about arrays. The concept is simple, however, the execution for this particular concept has been difficult for me. Everything is working as intended, however, I need to figure out a way that I can make the function push the next value into the output array.

---
+++ Full Stack Dev Code/JavaScript/FizzBuzz.js
@@ -0,0 +1,17 @@
+var output = [];
+
+function fizzBuzz() {
+    
+var numberOfKids = output.length;
+    
+output.push(numberOfKids);
+
+console.log(output.length);
+
+}
+
+
+
+fizzBuzz();
+
+console.log(output);
