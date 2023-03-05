bc368d5668faa563e183c610054b5ac35497d152
Mattie Britt <MattieBritt@users.noreply.github.com>
2023-03-04 19:43:28 -0700

    03/04/2023, Updated Files - FizzBuzz Part 2 Complete
    The second part of the FizzBuzz function is complete (where each number divisible by 3 had to push "Fizz" into the array, each number divisible by 5 had to push "Buzz" into the array, and each number divisible by both had to push "FizzBuzz" into the array). I struggled much less with the second part of the function, as it seemed fairly apparent to me that I would have to use an "If Else" statement.
    
    Starting tomorrow, I'll be dedicating time specifically away from my learning in order to practice and get better with not only the subjects I've learned so far (HTML, CSS, JS), but also room for future subjects as well.

--- Full Stack Dev Code/JavaScript/FizzBuzz.js
+++ Full Stack Dev Code/JavaScript/FizzBuzz.js
@@ -7,7 +7,30 @@ function fizzBuzz() {
 output.push(numberOfKids);
 
 numberOfKids++;
+    
 
+    if (numberOfKids % 3 == 0 ) {
+
+        output.push("Fizz")
+        
+    }
+
+    if (numberOfKids % 5 == 0) {
+
+        output.push("Buzz")
+    }
+    
+
+    if (numberOfKids % 3 == 0 && numberOfKids % 5 == 0) {
+
+        output.push("FizzBuzz")
+        output.pop("Buzz")
+        output.pop("Fizz")
+
+    }
+
+    
     console.log(output);
-
+    
 }
+
