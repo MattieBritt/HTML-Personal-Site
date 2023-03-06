d0b13c9d268c1958be8e9d8ae253b1fe5a5fa79b
Mattie Britt <MattieBritt@users.noreply.github.com>
2023-03-05 20:42:57 -0700

    03/05/2023 - Updated Files, Final FizzBuzz Version
    I finally finished the FIzzBuzz challenge, and the second part was much easier than the first. Here is the final version of the code. If there's something I've learned, it's that the order of your code matters!! Your code can be fine (and/or you have the right theory) but if your order is wrong it can completely wreck your code.

--- Full Stack Dev Code/JavaScript/FizzBuzz.js
+++ Full Stack Dev Code/JavaScript/FizzBuzz.js
@@ -3,34 +3,37 @@ var output = [];
 var numberOfKids = 1;
 
 function fizzBuzz() {
-        
+
 output.push(numberOfKids);
 
-numberOfKids++;
-    
+if (numberOfKids % 3 == 0 ) {
 
-    if (numberOfKids % 3 == 0 ) {
-
+        output.pop(numberOfKids)
         output.push("Fizz")
-        
+
     }
+    
 
     if (numberOfKids % 5 == 0) {
 
+        output.pop(numberOfKids)
         output.push("Buzz")
-    }
+    }``
     
 
     if (numberOfKids % 3 == 0 && numberOfKids % 5 == 0) {
 
+        output.pop(numberOfKids)
         output.push("FizzBuzz")
-        output.pop("Buzz")
-        output.pop("Fizz")
+
 
     }
-
     
-    console.log(output);
+        console.log(output);
+
+    numberOfKids++;
     
 }
+
+
 
